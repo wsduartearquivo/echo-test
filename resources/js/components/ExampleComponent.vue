dfbd2851e2ab87ff723988ac2645f0467ef43d8f
williamsduarte <williamduarteoficial@gmail.com>
2019-08-29 21:39:09 -0400

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Components</div>
+
+                    <div class="card-body" v-for="m in messages">
+                        {{ m.data }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+            return {
+                messages: []
+            }
+        },
+
+        mounted() {
+
+
+            window.Echo.channel('test-event')
+            .listen('ExampleEvent', (e) => {
+
+                this.messages.push(e);
+            });
+
+        }
+    }
+</script>
